2bcf107cffae632128b8f7770105239a3312ef32
Bruno Martins <bscmartins@gmail.com>
2012-11-26 00:40:14 +0000

    small fix on docs

--- Net-DiffBot/lib/Net/DiffBot.pm
+++ Net-DiffBot/lib/Net/DiffBot.pm
@@ -30,7 +30,7 @@ This module is just an interface for www.diffbot.com API.
     use Net::DiffBot;
 
     my $d = Net::DiffBot->new('token' => 'diffbottoken');
-	my $page_date = $d->get_data_from_url($url)
+	my $page_data = $d->get_data_from_url($url)
     ...
 
 
@@ -58,11 +58,11 @@ sub new {
   return $self
 }
 
-=head2 get_date_from_url
+=head2 get_data_from_url
 
 Fetch diffbot data based on the url , along with the url you can set other options
 
-	my $page_date = $d->get_data_from_url($url, 'tags' => 1, summary => 1)
+	my $page_data = $d->get_data_from_url($url, 'tags' => 1, summary => 1)
 
 	Valid flags are: callback, html, dontStripAds, tags, comments, summary
 	You can see the use of theses flags at www.diffbot.com
@@ -71,7 +71,7 @@ Fetch diffbot data based on the url , along with the url you can set other optio
 
 =cut
 
-=head2 get_date_by_content
+=head2 get_data_from_content
 
 Fetch diffbot data based on sent content , you also need to send the url and the content type  ('text/plain', 'text/html'). You can also set other options as with get_data_from_url.
 
