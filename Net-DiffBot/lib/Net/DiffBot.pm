961a28ea9ff38cde343553a2a16c273065058f2d
Bruno Martins <bscmartins@gmail.com>
2012-11-26 00:43:34 +0000

    Changed version

--- Net-DiffBot/lib/Net/DiffBot.pm
+++ Net-DiffBot/lib/Net/DiffBot.pm
@@ -16,11 +16,11 @@ Net::DiffBot - Interface to the diffbot.com API
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
@@ -191,14 +191,6 @@ sub build_request_url {
 =head1 AUTHOR
 
 Bruno Martins, C<< <bscmartins at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-net-diffbot at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-DiffBot>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
 
 
 =head1 SUPPORT
